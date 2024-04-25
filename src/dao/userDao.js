1d96a539a6ec2bc09491f3abdc4468b1e68d96db
Aura-HY <144097076+Aura-HY@users.noreply.github.com>
2024-04-25 11:28:52 +0800

    login\regiser

--- src/dao/userDao.js
+++ src/dao/userDao.js
@@ -5,7 +5,8 @@ exports.getWordDetal = async (wordName) => {
     SELECT 
         word_id AS wordId,
         word_name AS wordName,
-        word_meaning AS wordMeaning
+        word_meaning AS wordMeaning,
+        img
     FROM 
         word
     WHERE
@@ -27,4 +28,28 @@ exports.getWord = async () => {
             word
     `;
     return await db.query(sql);
+};
+
+exports.addUser = async (userId, password) => {
+    const sql = `
+        INSERT INTO
+            user_basic_table (user_id, password) 
+        VALUES
+            (?, ?)
+    `;
+    const sqlParams = [userId, password];
+    return await db.query(sql, sqlParams);
+};
+
+exports.getUserPassword = async (userId) => {
+    const sql = `
+        SELECT
+            password
+        FROM
+            user_basic_table
+        WHERE
+            user_id = ?
+    `;
+    const sqlParams = [userId];
+    return await db.query(sql, sqlParams);
 };
